0fc93bdbc6a6ae3d8279403c16ada56544f1d9a1
Wind Hong <hzywind@gmail.com>
2024-03-13 17:49:04 +0800

    Add Kafka producer and consumer beans and command line runner

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,19 @@
 package com.example.demo;
 
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.Scanner;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +22,63 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public KafkaProducer<String, String> kafkaProducer() {
+		Properties props = new Properties();
+    	props.put("bootstrap.servers", "localhost:9092");
+
+    	props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    	props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("client.id", "kafka-producer");
+		props.put("compression.type", "snappy");
+		props.put("acks", "all");
+		props.put("retries", 3);
+		props.put("max.in.flight.requests.per.connection", 1);
+		props.put("enable.idempotence", true);
+		props.put("transactional.id", "tx-id");
+		return new KafkaProducer<>(props);
+	}
+
+	@Bean
+	public KafkaConsumer<String, String> kafkaConsumer() {
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "localhost:9092");
+		props.put("group.id", "test-group");
+		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.put("auto.offset.reset", "latest");
+		props.put("enable.auto.commit", "false");
+		props.put("isolation.level", "read_committed");
+		return new KafkaConsumer<>(props);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(KafkaConsumer<String, String> kafkaConsumer, 
+		KafkaProducer<String, String> kafkaProducer) {
+		return args -> {
+			new Thread(() -> {
+				kafkaConsumer.subscribe(Collections.singletonList("test-topic"));
+				while (true) {
+					ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(100));
+					for (ConsumerRecord<String, String> record : records) {
+						System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>" + record.value());
+					} 
+					// Manually commit the offset of the last message processed
+					kafkaConsumer.commitSync();
+				}
+			}).start();
+
+			kafkaProducer.initTransactions(); // Initialize transactions
+			try (Scanner scanner = new Scanner(System.in)) {
+				while (true) {
+					kafkaProducer.beginTransaction(); // Start a new transaction
+					String message = scanner.nextLine();
+					kafkaProducer.send(new ProducerRecord<>("test-topic", "Kafka: " + message));
+					kafkaProducer.commitTransaction(); // Commit the transaction
+				}
+			} catch (Exception e) {
+				kafkaProducer.abortTransaction(); // Abort the transaction in case of an exception
+			}
+		};
+	}
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
+@EnableKafka
+@EmbeddedKafka(
+    partitions = 1, 
+    controlledShutdown = false,
+    brokerProperties = {
+        "listeners=PLAINTEXT://localhost:3333", 
+        "port=3333"
+})
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+    EmbeddedKafkaBroker kafkaEmbeded;
+
 	@Test
 	void contextLoads() {
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>" + kafkaEmbeded.getBrokersAsString());
+
 	}
 
 }
